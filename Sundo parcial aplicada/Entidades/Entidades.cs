111b72aea4a082aa3aa8fc162b17a2faa909e1c0
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-07-06 18:21:04 -0400

    añadi la clase entidades

--- Sundo parcial aplicada/Entidades/Entidades.cs
+++ Sundo parcial aplicada/Entidades/Entidades.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Sundo_parcial_aplicada.Entidades
 {
-    class Entidades
+    public class Entidades
     {
+        [Key]
+        public int Proyectoid { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Descripcion { get; set; }
     }
 }
